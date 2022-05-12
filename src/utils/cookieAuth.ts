80929bb0610764c7f3e826ada8a84fc18da62092
Prayuj Pillai <30574783+prayuj@users.noreply.github.com>
2022-05-12 17:24:07 +0530

    minor update

--- src/utils/cookieAuth.ts
+++ src/utils/cookieAuth.ts
@@ -1,11 +1,12 @@
 import { responseSchema } from "../types";
 
 const isAuthenticatedWithCookie = (cookies: string) => {
+    if (!cookies) return false;
     return cookies.includes(`key=${process.env.COOKIE_ACCESS_TOKEN}`);
 };
 
 const withCookieAuthenticator = async (event, context, callBack): Promise<responseSchema> => {
-    if (!isAuthenticatedWithCookie(event.headers.Cookie)) 
+    if (!isAuthenticatedWithCookie(event.headers?.Cookie)) 
         return { statusCode: 401, body: JSON.stringify({ message: 'Unauthorized' }) };    
 
     return await callBack(event, context);
